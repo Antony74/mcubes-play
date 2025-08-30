2d44b55a0ce76d3613530c5301407ca7ba9df2bf
Antony74 <akb@akb.me.uk>
2025-08-30 20:39:13 +0100

    Model a hollow cylinder

--- sdf.py
+++ sdf.py
@@ -1,4 +1,31 @@
 import math
 
+def createSurface(fn, depth):
+    def surface(x, y, z):
+        return abs(fn(x, y, z)) - depth                
+
+    return surface
+
+def cylinderWidth(y):
+    return 1
+
+def createTruncateY(fn, start, end):
+    def truncateY(x, y, z):
+        if (y >= start and y <= end):
+            return fn(x, y, z)
+        else:
+            return 10
+
+    return truncateY
+
+def createCylinder(fn):
+    def cylinder(x, y, z):
+        return (math.sqrt(x**2 + z**2) - fn(y))
+
+    return cylinder
+
 def sdf(x, y, z):
-    return -(math.sqrt(x**2 + y**2 + z**2) - 1.0)
+    cylinder = createCylinder(cylinderWidth)
+    surface = createSurface(cylinder, 0.1)
+    truncateY = createTruncateY(surface, -1.5, 1.5)
+    return -truncateY(x, y, z)
