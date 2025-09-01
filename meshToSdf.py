ce7f2ad7490a56b416dfc8c6363403253cfb1184
Antony74 <akb@akb.me.uk>
2025-09-01 09:37:13 +0100

    Include resolution in output filename

--- meshToSdf.py
+++ meshToSdf.py
@@ -6,8 +6,11 @@ import skimage
 print('Load mcup.stl')
 mesh = trimesh.load('mcup.stl')
 
+resolution = 256
+filename = 'mcup-resampled-{resolution}.stl'.format(resolution=resolution)
+
 print('mesh_to_voxels')
-voxels = mesh_to_voxels(mesh, 128, pad=True)
+voxels = mesh_to_voxels(mesh, resolution, pad=True)
 
 print('marching_cubes')
 vertices, faces, normals, _ = skimage.measure.marching_cubes(voxels, level=0)
@@ -15,8 +18,8 @@ vertices, faces, normals, _ = skimage.measure.marching_cubes(voxels, level=0)
 print('Trimesh')
 mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_normals=normals)
 
-print('Save mcup-resampled.stl')
-mesh.export('mcup-resampled.stl')
+print('Save ' + filename)
+mesh.export(filename)
 
 print('Show')
 mesh.show()
